b81553d45c46ec884155c39cddba30286b5ef74b
Abdul Wahab Asif <136114691+abdulwahab5547@users.noreply.github.com>
2024-09-01 11:47:34 +0500

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -22,25 +22,29 @@ const openai = new OpenAI({
 const port = process.env.PORT || 5000;
 
 const router = Router();
-app.use(json());
-app.use(_json());
 
+
+// Allowed origins
 const allowedOrigins = ['http://localhost:3000', 'https://buzz-theta.vercel.app'];
 
+// Configure CORS
 app.use(cors({
     origin: function(origin, callback) {
-        // Allow requests with no origin (like mobile apps or curl requests)
-        // or ones that are in the allowedOrigins array
-        if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+        if (!origin || allowedOrigins.includes(origin)) {
             callback(null, true);
         } else {
             callback(new Error('Not allowed by CORS'));
         }
-    }
+    },
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true // If you need to allow cookies or authentication
 }));
 
 app.use('/api', router);
 
+app.use(json());
+app.use(_json());
 
 // Multer
 
